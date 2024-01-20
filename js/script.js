56a957136e2402562b6b395c65b37777e0b86dd6
leelingyan98 <58385937+leelingyan98@users.noreply.github.com>
2024-01-20 22:52:39 +0800

    Added CSS class 'default' and array for tracking

--- js/script.js
+++ js/script.js
@@ -2,7 +2,38 @@ const gameBoard = document.getElementById('gameBoard');
 const cells = document.querySelectorAll('.cell');
 const statusText = document.getElementById('statusText');
 const restartButton = document.getElementById('restartButton');
-let currentPlayer = 'Black';
+
+const blackPlayer = 2; // Assign numerical value "2" to player Black
+const whitePlayer = 1; // Assign numerical value "1" to player Black
+let currentPlayer = 2; // Start with Black player
+
+console.log(cells);
+
+// Define game board layout with array
+const gameBoardLayout = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+function drawLayout() {
+    // Loop through each row to access layout values
+    for (let row = 0; row < gameBoardLayout.length; row++) {
+        const rowValue = gameBoardLayout[row];
+        console.log(rowValue);
+
+        //Loop through each column to access layout values
+        for (let column = 0; column < rowValue.length; column++) {
+            const colValue = rowValue[column];
+            console.log(colValue);
+        }
+    }
+}
 
 // How do I restart the game?
 function restartGame() {
@@ -16,6 +47,7 @@ function restartGame() {
 }
 
 // Initialize the game
+drawLayout();
 // gameBoard.addEventListener('click', handleCellClick); // run handleCellClick everytime gameBoard is clicked
 restartButton.addEventListener('click', restartGame); // run restartGame when Restart button is clicked
 restartGame();
